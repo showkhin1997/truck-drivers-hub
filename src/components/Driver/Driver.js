7e5302e59537c1d1f5e8f76c8b697d80d9b63e21
showkhin1997 <showkhin15@gmail.com>
2021-09-26 12:53:19 +0600

    rating dynamic

--- src/components/Driver/Driver.js
+++ src/components/Driver/Driver.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import './Driver.css'
+import Rating from 'react-rating';
 
 
 const Driver = (props) => {
     console.log(props.driver)
     const { name, age, img, city, rating, salary } = props.driver;
+    const buttonIcon = <FontAwesomeIcon icon={faUserPlus} />
+
     return (
         <div className="driver-card">
             <img className="card-image" src={img} alt="" />
             <div className="driver-details">
                 <h2>{name}</h2>
-                <h4>Age: {age}</h4>
+                <p>Age: {age}</p>
                 <p>City: {city}</p>
-                <p>Rating: {rating}</p>
-                <h3>Salary: {salary}</h3>
-                <button>Hire Now</button>
+                <p><Rating
+                    initialRating={rating}
+                    emptySymbol="far fa-star rating-icon"
+                    fullSymbol="fas fa-star rating-icon"
+                    readonly
+                ></Rating> </p>
+                <h3><small>Salary:</small> ${salary}</h3>
+                <button>{buttonIcon} <span className="button-text">Hire Now</span> </button>
             </div>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className="header-container">
             <h1 className="header">Truck <span>Driver's</span> Hub</h1>
+            <h2 className="header-text">"Choose Your Driver and Hire Them"</h2>
             <nav>
                 <a href="/home">Home</a>
                 <a href="/markets">Markets</a>
@@ -12,8 +13,8 @@ const Header = () => {
                 <a href="about">About us</a>
                 <input type="text" placeholder="search your driver" />
                 <button>Search</button>
-                <p className="total-bugget">Total Bugget: $100000</p>
             </nav>
+            <p className="total-bugget">Total Bugget: $100000</p>
         </div>
     );
 };
